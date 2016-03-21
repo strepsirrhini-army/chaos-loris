14f5efbe69a13757ab0060e1422ba2a67bab252e
Ben Hale <bhale@pivotal.io>
2016-03-20 20:51:00 -0700

    Updates to CI

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -4,7 +4,7 @@ set -e
 
 mysql_install_db --user=mysql --rpm
 mysqld_safe --datadir='/var/lib/mysql' &
-mysqladmin -u root password 'password'
+# mysqladmin -u root password 'password'
 
 cd chaos-loris
 ./mvnw -q package
