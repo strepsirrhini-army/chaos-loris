a3ca4df6669728dc7195d1ace4d8eb3e82c1ee9f
Ben Hale <bhale@pivotal.io>
2016-03-20 20:42:35 -0700

    Updates to CI

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -5,8 +5,5 @@ set -e
 mysql_install_db --user=mysql --rpm
 mysqld_safe --datadir='/var/lib/mysql' &
 
-# /usr/bin/mysqladmin -u root password 'password'
-# - SPRING_DATASOURCE_USERNAME=travis SPRING_DATASOURCE_PASSWORD=""
-
 cd chaos-loris
 ./mvnw -q package
