c996cc63bc4a084e14e0434988bfd481004ad839
Ben Hale <bhale@pivotal.io>
2016-03-20 21:06:45 -0700

    Updates to CI

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -4,7 +4,8 @@ set -e
 
 mysql_install_db --user=mysql --rpm
 mysqld_safe --datadir='/var/lib/mysql' &
-mysql -e "create database IF NOT EXISTS chaos-loris;" -uroot
+sleep 2
+mysqladmin create chaos-loris
 
 cd chaos-loris
 ./mvnw -q package
