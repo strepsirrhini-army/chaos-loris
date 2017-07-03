94d7fde2a23559a26346561147a3c28c2186761f
Ben Hale <bhale@pivotal.io>
2017-07-03 15:14:30 +0100

    Add Concourse Caching

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,5 +2,7 @@
 
 set -e -u
 
+M2=$PWD/m2
+
 cd chaos-loris
 ./mvnw -q -Dmaven.repo.local=$M2/repository -Dmaven.user.home=$M2 -Dmaven.test.skip=true deploy
--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -6,6 +6,8 @@ mysqld_safe --datadir='/var/lib/mysql' &
 sleep 2
 mysqladmin create chaos-loris
 
+M2=$PWD/m2
+
 cd chaos-loris
 ./mvnw -q -Dmaven.repo.local=$M2/repository -Dmaven.user.home=$M2 package
 
