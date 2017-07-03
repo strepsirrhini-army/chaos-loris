6736db1a628bc866356a25d5dbc7bb88de4520f1
Ben Hale <bhale@pivotal.io>
2017-07-03 15:51:27 +0100

    Add Concourse Caching

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -6,10 +6,7 @@ mysqld_safe --datadir='/var/lib/mysql' &
 sleep 2
 mysqladmin create chaos-loris
 
-export BUNDLE_PATH=$PWD/gems
-M2=$PWD/m2
-
 cd chaos-loris
-./mvnw -q -Dmaven.repo.local=$M2/repository -Dmaven.user.home=$M2 package
+BUNDLE_PATH=../gems ./mvnw -q -Dmaven.repo.local=../m2/repository -Dmaven.user.home=../m2 package
 
 mysqladmin shutdown
