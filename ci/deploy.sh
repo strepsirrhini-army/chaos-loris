83d24803976cf72e99dfd75bdb06cf6ab83b7a06
Ben Hale <bhale@pivotal.io>
2017-07-04 13:51:56 +0100

    Simpler Concourse Caching

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,7 +6,9 @@ mysqld_safe --datadir='/var/lib/mysql' &
 sleep 2
 mysqladmin create chaos-loris
 
+ln -fs m2 ~/.m2
+
 cd chaos-loris
-./mvnw -q -Dmaven.repo.local=../m2/repository -Dmaven.user.home=../m2 deploy
+./mvnw -q deploy
 
 mysqladmin shutdown
